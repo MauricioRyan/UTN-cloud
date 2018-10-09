26f904e99cee3b177d94c3c60ccab5ed39f3617c
minikube <mauricioryan@gmail.com>
2018-10-09 13:00:34 -0300

    arreglado

--- pLiveRedy/comando.bash
+++ pLiveRedy/comando.bash
@@ -29,22 +29,24 @@ kubectl run alpaca-prod --image=gcr.io/kuar-demo/kuard-amd64:1 --replicas=3 --po
 kubectl apply -f alpaca-prod-deployment.yaml
 
 kubectl expose deployment alpaca-prod --port=8080 --target-port=8080 --dry-run -oyaml > alpaca-prod-service.yaml
-# editar y agregar dentre de spec:
+# editar y agregar dentro de spec:
 # type: NodePort 
 
+#para saber que URL tiene el servicio....
 minikube service alpaca-prod --url
 
 
-#terminal 2
+#terminal 1
 kubectl get endpoints alpaca-prod --watch
 
-#terminal 3
+#terminal 2
 kubectl get pods 
 
-#terminal 1
-kubectl port-forward unPodDelTerminal3 8888:8080
-
-#teminal 4
-#hacer un curl porque sino no se ve que cambia el pod que sirve, por el cache del browser
+#terminal 3
+#hacer un curl porque sino no se ve que cambia el pod que sirve, por el cache del browser usando el URL del servicio
 #curl http://192.168.39.207:30207 |grep host
 #repetir para ver como rota el pod que atiende
+
+#terminal 4
+kubectl port-forward unPodDelTerminal3 8888:8080
+#para acceder al pod y ponerlo en falla y/o muerto
